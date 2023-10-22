30263671bb68e220b9583b177a7b59f1974c81fa
Marcos Valeriano <marcosfov@gmail.com>
2023-10-22 18:21:26 -0300

    feat: add second banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -28,8 +28,18 @@ export default async function Home() {
       </div>
 
       <div className="mt-8">
+        <p className="font-bold uppercase mb-3 pl-5">Ofertas</p>
         <ProductList products={deals} />
       </div>
+
+      <Image
+        src="/banner-home-02.png"
+        height={0}
+        width={0}
+        className="h-auto w-full p-5"
+        sizes="100vw"
+        alt="Até 55% de desconto em mouses!"
+      />
     </div>
   );
 }
