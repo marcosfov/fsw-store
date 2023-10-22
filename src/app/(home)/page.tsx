ea3ef5a6e0722987ceda829853ed2ec8ee0bb114
Marcos Valeriano <marcosfov@gmail.com>
2023-10-22 18:40:49 -0300

    feat: add third banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -23,17 +23,17 @@ export default async function Home() {
   });
 
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       <PromoBanner
         src="/banner-home-01.png"
         alt="Até 55% de desconto esse mês!"
       />
 
-      <div className="mt-8 p-5">
+      <div className=" p-5">
         <Categories />
       </div>
 
-      <div className="mt-8">
+      <div>
         <SectionTitle>Ofertas</SectionTitle>
         <ProductList products={deals} />
       </div>
@@ -43,10 +43,15 @@ export default async function Home() {
         alt="Até 55% de desconto em mouses!"
       />
 
-      <div className="mt-8">
+      <div>
         <SectionTitle>Teclados</SectionTitle>
         <ProductList products={keyboards} />
       </div>
+
+      <PromoBanner
+        src="/banner-home-03.png"
+        alt="Até 20% de desconto em fones!"
+      />
     </div>
   );
 }
